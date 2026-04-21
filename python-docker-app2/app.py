e474a4a18e489a6e0a077a357b0554fb134a99ab
Mihitha01 <mihithabandara@gmail.com>
2026-04-21 09:43:18 +0530

    Update Flask response message and add VSCode settings for Python environment management

--- python-docker-app2/app.py
+++ python-docker-app2/app.py
@@ -4,7 +4,8 @@ app = Flask(__name__)
 
 @app.get("/")
 def home():
-    return {"Hello from Python inside Docker!"}
+    #return {"Hello from Python inside Docker!"}
+    return {"message": "Hello from Python inside Docker!"}
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
